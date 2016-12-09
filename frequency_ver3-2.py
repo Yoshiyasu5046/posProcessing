9a6aa5f593c70ae081783a959e59a8c53f9407cf
Yoshiyasu Kitahara <yoshi.kitahara.niapat@gmail.com>
2016-12-09 11:31:28 +0900

    no message

--- frequency_ver3-2.py
+++ frequency_ver3-2.py
@@ -1,7 +1,6 @@
 # bin/python
 # -*- coding: utf-8 -*-
 
-# import re
 
 inputted_file = open('output1.txt', 'r')
 outputted_file1 = open('output3.txt', 'w')
@@ -12,6 +11,7 @@ outputted_file1 = open('output3.txt', 'w')
 # 利用するべきかをわかった上で用いるべき。
 # 開いたファイルを閉じるタイミング。そもそも閉じる必要はあるのか。そうだとするといつ閉じるべきか。
 # 膨大なデータを扱う際、このような配列を利用した処理は使用できるのか疑問。
+
 arrayOfOverlapped = []
 for line in inputted_file:
 	outputted_file2 = open('output2.txt', 'r+')
@@ -21,12 +21,18 @@ for line in inputted_file:
 			arrayOfOverlapped.append(line[:22])
 	if line[:22] not in arrayOfOverlapped:
 		outputted_file2.write(line)
-	
-	# sample = re.search(line, outputted_file2, re.MULTILINE)
-	# print sample
-	# outputted_file1.write(line)
-	
-	# outputted_file2.write(line)
+
+
+
+# arrayOfOverlapped = []
+# for line in inputted_file:
+# 	outputted_file2 = open('output2.txt', 'r+')
+# 	for item in outputted_file2:
+# 		if item[:22] == line[:22]:
+# 			outputted_file1.write(line)
+# 			arrayOfOverlapped.append(line[:22])
+# 	if line[:22] not in arrayOfOverlapped:
+# 		outputted_file2.write(line)
 
 
 inputted_file.close()
